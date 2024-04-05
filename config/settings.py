71f5e5534e5eebb6331ac2b5affa0dd5baebc365
Alexander Petricca <petchalxande@gmail.com>
2024-04-05 16:30:26 +0100

    Added API functionality and configured cors headers in line with development with React frontend.

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+
+    # Third Party
+    'rest_framework',
+    'corsheaders',
 
     # Local
     'todos.apps.TodosConfig',
@@ -45,6 +50,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,8 +126,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
+# Cors Configuration
+
+CORS_ALLOWED_ORIGINS = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
+
+CSRF_TRUSTED_ORIGINS = ["https://localhost:3000"]
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+
+
+    @classmethod
+    def setUpTestData(cls):
+
+        cls.todo = Todo.objects.create(
+            title = 'First Todo',
+            body = 'Some useful supporting text.',
+        )
+
+
+    def testModelContent(self):
+        """
+        Performs basic tests on the Todo model.
+        """
+
+        self.assertEqual(self.todo.title, 'First Todo')
+        self.assertEqual(self.todo.body, 'Some useful supporting text.')
+        self.assertEqual(str(self.todo), 'First Todo')
+
+
+    def testApiListView(self):
+        """
+        Tests the todo list endpoint.
+        """
+        
+        response = self.client.get(reverse('todo_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+
+    def testApiDetailView(self):
+        """
+        Tests the todo detail (single object) endpoint.
+        """
+
+        response = self.client.get(reverse('todo_detail', kwargs={'pk': self.todo.id}), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, 'First Todo')
